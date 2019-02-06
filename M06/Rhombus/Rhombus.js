acc0bcc1498baddee2d3aeccfe61f7813d5ecbe0
Caryswebsite1 <46387153+Caryswebsite1@users.noreply.github.com>
2019-02-06 12:32:43 -0800

    Final updates
    changed up Right and down Right in css as well as in js.
    changed background color of color input show boxes to that of container
    added comments and headers to functions in js.  Converted createRhombus function to be just one function. Incorporated the upRight and downRight functions into it.

--- M06/Rhombus/Rhombus.js
+++ M06/Rhombus/Rhombus.js
@@ -1,14 +1,10 @@
 // JavaScript source code
 "use strict";
 
-// var rHeight =5;
-// var colorEven = "orange";
-// var colorOdd = "black";
-// var symbol ="*";
+// set up init event so we can have an init function
 window.addEventListener("load", init);  // so we can do some init stuff.
 
-
-
+// selection events so we can change the color of the color input displays
 var EvenSelector = document.querySelector("#colorEven");
 EvenSelector.addEventListener("input", showEvenColorHandler, false);
 
@@ -16,12 +12,23 @@ var OddSelector = document.querySelector("#colorOdd");
 OddSelector.addEventListener("input", showOddColorHandler, false);
 
 
+// ----------------------------------------------------------
+//  Function init
+//  called by load handler
+//  Used to set initial stuff. In this case, the color input display boxes.
+// ----------------------------------------------------------
 function init() {
     // set the initial colors in the colorboxes so they show.
     showEvenColorHandler();
     showOddColorHandler();
 }
 
+
+// ----------------------------------------------------------
+//  Function showEvenColorHandler
+//  called by init and event handler
+//  Used to set the even color input display box.
+// ----------------------------------------------------------
 function showEvenColorHandler() {
     var colorInput = "";
     var colorSelector = document.getElementById("colorEven");
@@ -33,6 +40,11 @@ function showEvenColorHandler() {
 }
 
 
+// ----------------------------------------------------------
+//  Function showOddColorHandler
+//  called by init and event handler
+//  Used to set the odd color input display box.
+// ----------------------------------------------------------
 function showOddColorHandler() {
     var colorInput = "";
     var colorSelector = document.getElementById("colorOdd");
@@ -44,114 +56,81 @@ function showOddColorHandler() {
 }
 
 
-
+// ----------------------------------------------------------
+//  Function createRhombus
+//  called by display rhombus button click
+//  Creates multiline rhombus and "prints" to the upside and downside
+//  elements in the page.
+// ----------------------------------------------------------
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
-    upRight(pHeight, pColorEven, pColorOdd, pSymbol);
-    downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+ 
+// NOTE:  we are going to use text align to center each line so no worries about spaceing.
 
-}
-
-function upRight(pHeight, pColorEven, pColorOdd, pSymbol) {
+// do upside part first
     var rLine = "";
 
     // NOTE:  this doesn't seem to work.  this backgroundColor prop is never set via the .css 
     // though it works via the .css.  If you set this style.backgroundColor, it will override the css file
     // and show the color.  Unfortunately it doesn not initially have the actual background color.
-    var backColor = document.querySelector("body").style.backgroundColor;
+   // var backColor = document.querySelector("body").style.backgroundColor;
 
-    backColor = "white";  // setting to white for now.
-
-   // var startPosition = pHeight;;  // when to actually start printing color for this line
+   
     var i = 0;
     var j = 0;
-    var z = 0;
 
     for (i = 0; i < pHeight; i++) {
         rLine += "<p>";
-       
-
  
-        //console.log("upright startPosition = " + startPosition + "  i = " + i);
-
-        // first go out to the start Color position:  Works best for odd heights but ok for even.
-      /*  for (z = 0; z < startPosition; z++) {
-            rLine += "<span style='color:" + backColor + ";'>" + pSymbol + "</span>";
-        }
-        */
-
-        // now at the right position to start printing.
-
         //Create each line on the Rhombus
         for (j = 0; j <= (2*i); j++) {
 
             //Is the position even or odd so we change the color
-            if (j % 2)
+            if (j % 2) {
                 //even
                 rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-            else
+            }
+            else {
                 //odd
                 rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+            }
+        } // end for j
 
-        }
         rLine += "</p>";
-        // console.log(rLine);
 
-       // startPosition -= 1; // decrement by 1
-    }
+    }// end for i 
 
-    document.getElementById("upRight").innerHTML = rLine;
-}
+    // print to page for upside:
+    document.getElementById("upSide").innerHTML = rLine;
 
-function downRight(pHeight, pColorEven, pColorOdd, pSymbol) {
-    var rLine = "";
-    //var startPosition = 1;  // when to actually start printing color for this line
+    // ******************************************************************
+    // ***** now do the down side.  same idea just reverse i direction.
 
-    // NOTE:  this doesn't seem to work.  this backgroundColor prop is never set via the .css 
-    // though it works via the .css.  If you set this style.backgroundColor, it will override the css file
-    // and show the color.  Unfortunately it doesn not initially have the actual background color.
-    var backColor = document.querySelector("body").style.backgroundColor;
-
-    backColor = "white";  // setting to white for now.
-
-    var i = 0;
-    var j = 0;
-    var z = 0;
-
-    console.log("backColor = " + backColor);
-   
+    rLine = "";  // reset variables.
+    i = 0;
+    j = 0;
 
     for (i = (pHeight-1); i > -1; i--) {
         rLine += "<p>";
-
-        
-        //console.log("downright startPosition = " + startPosition + "  i = " + i);
-
-        // now at right position to start printing
-
-        // first go out to the start Color position:  Works best for odd heights but ok for even.
-   /*     for (z = 0; z < startPosition; z++) {
-            rLine += "<span style='color:" + backColor + ";'>" + pSymbol + "</span>";
-        }
-*/
-
+ 
         //Create each line on the Rhombus
         for (j = 0; j <= (2*i); j++) {
 
             //Is the position even or odd so we change the color
-            if (j % 2)
+            if (j % 2) {
                 //even
                 rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol + "</span>";
-            else
+            }
+            else {
                 //odd
                 rLine += "<span style='color:" + pColorOdd + ";'>" + pSymbol + "</span>";
+            }
+        }// end for j
 
-        }
         rLine += "</p>";
-        // console.log(rLine);
 
-        //startPosition += 1; // increment by 1
     }// end for i
 
-    document.getElementById("downRight").innerHTML = rLine;
+    // now print to page via downSide
+    document.getElementById("downSide").innerHTML = rLine;
 }
 
