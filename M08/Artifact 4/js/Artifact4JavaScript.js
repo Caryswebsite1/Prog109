af5a31a4b6aff40d3a9223517ae5bc82fe78d2e1
Caryswebsite1 <46387153+Caryswebsite1@users.noreply.github.com>
2019-02-23 13:27:27 -0800

    final changes to get it working.

--- M08/Artifact 4/js/Artifact4JavaScript.js
+++ M08/Artifact 4/js/Artifact4JavaScript.js
@@ -4,10 +4,10 @@
 
 // since some can only have alpha and some only numbers, set up match variables
 // via regular expressions:  ^ means find char NOT between the brackts. i means ignore case.
-let lettersAndNums = /[^0-9A-Z]/i;
 let lettersOnly = /[^A-Z]/i;
 let NumsOnly = /[^0-9]/;
 let emailChars = ["@", "."];  // these chars are required by email
+let PhoneFormat = /[^0-9-]/;
 
 
 
@@ -34,28 +34,29 @@ function ValidateForm() {
 
 
     // created another function to validate based on inputs:  
-    // arguments are: id of form input item, id of that items associated error item, max char, 
+    // arguments are: id of form input item, Standard text form of input item to print in error messages,
+    // id of that items associated error item, max char, 
     // Not allowed chartype, required char types, required, is this an array value from a selection input...
 
-    bFirstName = GenericFormValidator('FirstName', 'FirstNameError', 20, lettersOnly, null, true, false);
-    bLastName = GenericFormValidator('LastName', 'LastNameError', 50, lettersOnly, null, true, false);
-    bEmail = GenericFormValidator('Email', 'EmailError', 100, null, emailChars, true, false);
-    bPhone = GenericFormValidator('Phone', 'PhoneError', 15, NumsOnly, null, true, false);
-    bUserName = GenericFormValidator('UserName', 'UserNameError', 12, null, null, true, false);
-    bPassword = GenericFormValidator('Password', 'PasswordError', 7, null, null, true, false);
-    bStreetAdd = GenericFormValidator('StreetAddress', 'StreetAddError', 100, null, null, true, false);
-    bCity = GenericFormValidator('City', 'CityError', 100, null, null, true, false);
-    bState = GenericFormValidator('State', 'StateError', null, null, null, true, true);
-    bCountry = GenericFormValidator('Country', 'CountryError', null, null, null, true, true);
+    bFirstName = GenericFormValidator('FirstName', 'First Name', 'FirstNameError', 20, lettersOnly, null, true, false);
+    bLastName = GenericFormValidator('LastName', 'Last Name', 'LastNameError', 50, lettersOnly, null, true, false);
+    bEmail = GenericFormValidator('Email', 'Email', 'EmailError', 100, null, emailChars, true, false);
+    bPhone = GenericFormValidator('Phone', 'Phone', 'PhoneError', 15, PhoneFormat, null, true, false);
+    bUserName = GenericFormValidator('UserName', 'User Name', 'UserNameError', 12, null, null, true, false);
+    bPassword = GenericFormValidator('Password', 'Password', 'PasswordError', 7, null, null, true, false);
+    bStreetAdd = GenericFormValidator('StreetAddress', 'Street Address', 'StreetAddError', 100, null, null, true, false);
+    bCity = GenericFormValidator('City', 'City', 'CityError', 100, null, null, true, false);
+    bState = GenericFormValidator('State', 'State', 'StateError', null, null, null, true, true);
+    bCountry = GenericFormValidator('Country', 'Country', 'CountryError', null, null, null, true, true);
 
     // have to check country before sending zip.
     if (bCountry && document.getElementById("Country").value === "USA") {
         // zip is required.
-        bZip = GenericFormValidator('Zip', 'ZipError', 5, null, null, true, false);
+        bZip = GenericFormValidator('Zip', 'Zip Code', 'ZipError', 5, null, null, true, false);
     }
     else {
         // zip not required..
-        bZip = GenericFormValidator('Zip', 'ZipError', 5, null, null, false, false);
+        bZip = GenericFormValidator('Zip', 'Zip Code', 'ZipError', 5, null, null, false, false);
     }
 
    
@@ -63,7 +64,8 @@ function ValidateForm() {
     // Make sure you return all the boolean variables that are checking each field
     return (bFirstName && bLastName && bEmail && bPhone && bUserName &&
         bPassword && bStreetAdd && bCity && bState && bCountry && bZip);
-}
+
+}// end ValidateForm function
 
 
 //---------------------------------------------------------------------------------
@@ -72,8 +74,11 @@ function ValidateForm() {
 // Takes a number of input parameters to be able to validate input forms for 
 // if they are required, max length and character types allowed.  
 // Provides generic error message based on item id.
+// Arguments in order:
+// itemID: The html page id of the input or select element in question
+
 // ----------------------------------------------------------------------------------
-function GenericFormValidator(itemID, itemErrorID, MaxLength, NotAllowedCharType, ReqCharTypes, bRequired, bArray) {
+function GenericFormValidator(itemID, itemText, itemErrorID, MaxLength, NotAllowedCharType, ReqCharTypes, bRequired, bArray) {
 
     // set up error message element variable for the first name:
     let errMsgEl = document.getElementById(itemErrorID);
@@ -91,7 +96,7 @@ function GenericFormValidator(itemID, itemErrorID, MaxLength, NotAllowedCharType
         if (formItem.options[formItem.selectedIndex].value === null || formItem.options[formItem.selectedIndex].value === "000") {
             // user has not selected anything.
             bValidateOK = false;
-            errorMessage = "Oops! The " + itemID + " is required.";
+            errorMessage = "Oops! The " + itemText + " is required.";
         }
     }// end if an array value item
     else {
@@ -106,12 +111,12 @@ function GenericFormValidator(itemID, itemErrorID, MaxLength, NotAllowedCharType
         if (formItem.value.length < minLength) {
             // length is too small or none existant.
             bValidateOK = false;
-            errorMessage = "Oops! The " + itemID + " is required.";
+            errorMessage = "Oops! The " + itemText + " is required.";
         }
         else if (MaxLength !== null && formItem.value.length > MaxLength) {
             // Length too long!
             bValidateOK = false;
-            errorMessage = "Oops! The " + itemID + " has a maximum length of " + MaxLength + " characters.";
+            errorMessage = "Oops! The " + itemText + " has a maximum length of " + MaxLength + " characters.";
         }
         else if (formItem.value.length >= minLength &&
             MaxLength !== null &&
@@ -122,20 +127,17 @@ function GenericFormValidator(itemID, itemErrorID, MaxLength, NotAllowedCharType
                 bValidateOK = false;
 
                 switch (NotAllowedCharType) {
-                    case lettersAndNums:
-                        errorMessage = "Oops! The " + itemID + " can only be alpha-numeric characters.";
-                        break;
 
                     case lettersOnly:
-                        errorMessage = "Oops! The " + itemID + " can only be alphabetic characters.";
+                        errorMessage = "Oops! The " + itemText + " can only be alphabetic characters.";
                         break;
 
                     case NumsOnly:
-                        errorMessage = "Oops! The " + itemID + " can only be numeric characters.";
+                        errorMessage = "Oops! The " + itemText + " can only be numeric characters.";
                         break;
 
                     default:
-                        errorMessage = "Oops! The " + itemID + " can't use the type of characters you put in.";
+                        errorMessage = "Oops! The " + itemText + " can't use the type of characters you put in.";
                 }// end switch chartype
 
             }// end if chartype doesn't match
@@ -158,7 +160,7 @@ function GenericFormValidator(itemID, itemErrorID, MaxLength, NotAllowedCharType
                 if (!bAllThere) {
                     // need error message for not having the all the required charactors
                     bValidateOK = false;
-                    errorMessage = "Oops! The " + itemID + " doesn't look right.  Maybe you don't have everything you need?";
+                    errorMessage = "Oops! The " + itemText + " doesn't look right.  Maybe you don't have everything you need?";
                 }
 
             } // end required char check
